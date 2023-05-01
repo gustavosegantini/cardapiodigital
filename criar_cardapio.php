16ff64d39b43ed78cc003ec216fb8f48e4128c9d
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-05-01 20:21:17 -0300

    Atualização dos cardapios

--- criar_cardapio.php
+++ criar_cardapio.php
@@ -1,35 +1,21 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criar Cardápio</title>
-    <link rel="stylesheet" href="path/to/your/css/colors.css">
-    <link rel="stylesheet" href="path/to/your/css/cadastro_style.css">
-    <link rel="stylesheet" href="css/criar_cardapio_style.css">
-    <!-- Adicione o caminho correto para seus arquivos CSS -->
+    <link rel="stylesheet" href="path/to/your/css/criar_cardapio_style.css">
 </head>
-
 <body>
     <div class="container">
         <h1>Criar Cardápio</h1>
-        <form action="criar_cardapio_action.php" method="post" enctype="multipart/form-data">
+        <form action="criar_cardapio_action.php" method="post">
             <div class="form-group">
-                <label for="nome">Nome do Cardápio:</label>
-                <input type="text" name="nome" id="nome" required>
-            </div>
-            <div class="form-group">
-                <label for="descricao">Descrição do Cardápio (opcional):</label>
-                <textarea name="descricao" id="descricao"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="imagem">Imagem do Cardápio (opcional):</label>
-                <input type="file" name="imagem" id="imagem">
+                <label for="titulo">Título do Cardápio:</label>
+                <input type="text" name="titulo" id="titulo" required>
             </div>
             <input type="submit" value="Criar Cardápio">
         </form>
     </div>
 </body>
-
 </html>
--- criar_cardapio_action.php
+++ criar_cardapio_action.php
@@ -1,29 +1,30 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["restaurante_id"])) {
+// Verificar se o usuário está logado
+if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
-    exit();
+    exit;
 }
 
-require_once "../conect.php";
-$conn = conectarBD();
+require_once("conect.php");
 
-$restaurante_id = $_SESSION["restaurante_id"];
-$nome = $_POST["nome"];
-$descricao = $_POST["descricao"];
-$imagem = "";
+$restaurante_id = $_SESSION['user_id'];
+$titulo = $_POST['titulo'];
 
-// Processar o upload da imagem, se houver
-if (isset($_FILES["imagem"]) && $_FILES["imagem"]["error"] == 0) {
-    $imagem = "uploads/" . basename($_FILES["imagem"]["name"]);
-    move_uploaded_file($_FILES["imagem"]["tmp_name"], $imagem);
-}
-
-$sql = "INSERT INTO cardapios (restaurante_id, nome, descricao, imagem) VALUES (?, ?, ?, ?)";
+// Inserir o cardápio no banco de dados
+$sql = "INSERT INTO cardapios (restaurante_id, titulo) VALUES (?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("isss", $restaurante_id, $nome, $descricao, $imagem);
-$stmt->execute();
+$stmt->bind_param("is", $restaurante_id, $titulo);
 
-header("Location: dashboard.php");
+if ($stmt->execute()) {
+    // Redirecionar para o dashboard
+    header("Location: dashboard.php?msg=Cardápio criado com sucesso!");
+} else {
+    // Redirecionar para a página de criação de cardápio com mensagem de erro
+    header("Location: criar_cardapio.php?msg=Erro ao criar o cardápio. Por favor, tente novamente.");
+}
+
+$stmt->close();
+$conn->close();
 ?>
