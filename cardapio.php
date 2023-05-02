ae19c8730c6bea5a6647e0d962d1078e0d5a821c
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-05-02 12:05:20 -0300

    Create cardapio.php

---
+++ cardapio.php
@@ -0,0 +1,61 @@
+<?php
+require 'conect.php';
+
+function obterPratos()
+{
+    $conn = conectarBD();
+    $sql = "SELECT * FROM pratos";
+    $result = $conn->query($sql);
+
+    $pratos = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $pratos[] = $row;
+        }
+    }
+
+    $conn->close();
+
+    return $pratos;
+}
+
+$pratos = obterPratos();
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cardápio</title>
+    <!-- Inclua aqui o link para o CSS e qualquer outro recurso de estilo que você desejar -->
+</head>
+
+<body>
+    <h1>Cardápio</h1>
+    <div id="cardapio-container">
+        <?php foreach ($pratos as $prato): ?>
+            <div class="prato">
+                <h2>
+                    <?= $prato['nome'] ?>
+                </h2>
+                <p>
+                    <?= $prato['descricao'] ?>
+                </p>
+                <p>R$
+                    <?= number_format($prato['preco'], 2, ',', '.') ?>
+                </p>
+                <p>Categoria:
+                    <?= $prato['categoria'] ?>
+                </p>
+            </div>
+        <?php endforeach; ?>
+
+        <!-- Aqui vamos exibir os pratos do cardápio -->
+    </div>
+    <!-- Inclua aqui o link para o JavaScript e qualquer outro recurso de script que você desejar -->
+</body>
+
+</html>
