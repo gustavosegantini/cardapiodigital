eb97cc28ae7aad3499cb14e49dcab3085ce47bf5
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-05-02 12:40:39 -0300

    Design

--- cardapio.php
+++ cardapio.php
@@ -32,46 +32,43 @@ if (isset($_GET['restaurante_id'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/cardapio_style.css">
     <title>Cardápio</title>
 </head>
 
 <body>
     <h1>Cardápio</h1>
+
     <div class="container">
-        <h1 class="text-center mt-4 mb-4">Cardápio</h1>
+        <h1>Cardápio</h1>
         <div class="row">
             <?php foreach ($pratos as $prato): ?>
-                <div class="col-md-4 col-sm-6 col-12 mb-4">
-                    <div class="card">
-                        <?php if ($prato['imagem']): ?>
-                            <img src="<?php echo $prato['imagem']; ?>" class="card-img-top"
-                                alt="<?php echo htmlspecialchars($prato['nome']); ?>">
-                        <?php endif; ?>
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <?php echo htmlspecialchars($prato['nome']); ?>
-                            </h5>
-                            <p class="card-text">
-                                <?php echo htmlspecialchars($prato['descricao']); ?>
-                            </p>
-                            <p class="card-text">Preço: R$
-                                <?php echo number_format($prato['preco'], 2, ',', '.'); ?>
-                            </p>
-                            <p class="card-text">Categoria:
-                                <?php echo htmlspecialchars($prato['categoria']); ?>
-                            </p>
-                        </div>
-                    </div>
+                <div class="card">
+                    <?php if ($prato['imagem']): ?>
+                        <img src="<?php echo $prato['imagem']; ?>" alt="<?php echo htmlspecialchars($prato['nome']); ?>">
+                    <?php endif; ?>
+                    <h2 class="card-title">
+                        <?php echo htmlspecialchars($prato['nome']); ?>
+                    </h2>
+                    <p class="card-text">
+                        <?php echo htmlspecialchars($prato['descricao']); ?>
+                    </p>
+                    <p class="card-text">Preço: R$
+                        <?php echo number_format($prato['preco'], 2, ',', '.'); ?>
+                    </p>
+                    <p class="card-text">Categoria:
+                        <?php echo htmlspecialchars($prato['categoria']); ?>
+                    </p>
                 </div>
             <?php endforeach; ?>
         </div>
     </div>
+
 </body>
 
 </html>
