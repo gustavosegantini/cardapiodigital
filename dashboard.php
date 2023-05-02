7f0c08ff9b0d010ce9a96a03edf36432a7ec20cc
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-05-01 22:38:21 -0300

    S

--- dashboard.php
+++ dashboard.php
@@ -53,65 +53,79 @@
 
     <section>
         <h2>Pratos</h2>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#adicionarPratoModal">
-            + Adicionar Prato
-        </button>
-
+        <button id="adicionarPratoBtn">+ Adicionar Prato</button>
     </section>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-        integrity="sha384-oBqDVmMz4fnFO9gybB+XG/7CwFqlB6t8H6fX1lqFFudGq3DycvbG8I2Q4U+Soz6N"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
-        integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/"
-        crossorigin="anonymous"></script>
 
     <!-- Modal Adicionar Prato -->
-    <div class="modal fade" id="adicionarPratoModal" tabindex="-1" aria-labelledby="adicionarPratoModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="adicionarPratoModalLabel">Adicionar Prato</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div id="adicionarPratoModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h5>Adicionar Prato</h5>
+            <form action="adicionar_prato_action.php" method="post" enctype="multipart/form-data">
+                <div>
+                    <label for="nome">Nome</label>
+                    <input type="text" id="nome" name="nome" required>
                 </div>
-                <form action="adicionar_prato_action.php" method="post" enctype="multipart/form-data">
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label for="nome" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="nome" name="nome" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="descricao" class="form-label">Descrição</label>
-                            <textarea class="form-control" id="descricao" name="descricao" rows="3"></textarea>
-                        </div>
-                        <div class="mb-3">
-                            <label for="imagem" class="form-label">Imagem (opcional)</label>
-                            <input type="file" class="form-control" id="imagem" name="imagem">
-                        </div>
-                        <div class="mb-3">
-                            <label for="preco" class="form-label">Preço</label>
-                            <input type="number" step="0.01" class="form-control" id="preco" name="preco" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="categoria" class="form-label">Categoria</label>
-                            <select class="form-select" id="categoria" name="categoria" required>
-                                <option value="entrada">Entrada</option>
-                                <option value="petiscos">Petiscos</option>
-                                <option value="pratos principais">Pratos Principais</option>
-                                <option value="bebidas">Bebidas</option>
-                                <option value="sobremesas">Sobremesas</option>
-                                <option value="carta de vinhos">Carta de Vinhos</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                        <button type="submit" class="btn btn-primary">Adicionar Prato</button>
-                    </div>
-                </form>
-            </div>
+                <div>
+                    <label for="descricao">Descrição</label>
+                    <textarea id="descricao" name="descricao" rows="3"></textarea>
+                </div>
+                <div>
+                    <label for="imagem">Imagem (opcional)</label>
+                    <input type="file" id="imagem" name="imagem">
+                </div>
+                <div>
+                    <label for="preco">Preço</label>
+                    <input type="number" step="0.01" id="preco" name="preco" required>
+                </div>
+                <div>
+                    <label for="categoria">Categoria</label>
+                    <select id="categoria" name="categoria" required>
+                        <option value="entrada">Entrada</option>
+                        <option value="petiscos">Petiscos</option>
+                        <option value="pratos principais">Pratos Principais</option>
+                        <option value="bebidas">Bebidas</option>
+                        <option value="sobremesas">Sobremesas</option>
+                        <option value="carta de vinhos">Carta de Vinhos</option>
+                    </select>
+                </div>
+                <div>
+                    <button type="button"
+                        onclick="document.getElementById('adicionarPratoModal').style.display='none'">Fechar</button>
+                    <button type="submit">Adicionar Prato</button>
+                </div>
+            </form>
         </div>
     </div>
+
+    <script>
+        // Pega o modal
+        var modal = document.getElementById("adicionarPratoModal");
+
+        // Pega o botão que abre o modal
+        var btn = document.getElementById("adicionarPratoBtn");
+
+        // Pega o elemento <span> que fecha o modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // Quando o usuário clicar no botão, abra o modal
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
+
+        // Quando o usuário clicar no <span> (x), feche o modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        // Quando o usuário clicar em qualquer lugar fora do modal, feche-o
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
+
 </body>
 
 </html>
