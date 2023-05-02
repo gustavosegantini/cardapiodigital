477bd0094ccd5e83ad5d7d27a583932ff08c1f0e
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-05-01 23:59:26 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -90,7 +90,9 @@
                                 <?= $prato['categoria'] ?>
                             </td>
                             <td>
-                                <button class="btn btn-warning" data-prato-id="<?= $prato['id'] ?>">Editar</button>
+                                <button data-id="<?= $prato['id'] ?>" class="btn-editar"
+                                    onclick="editarPrato(event)">Editar</button>
+
                                 <button class="btn btn-danger" data-prato-id="<?= $prato['id'] ?>">Excluir</button>
                             </td>
                         </tr>
@@ -169,6 +171,52 @@
                 modal.style.display = "none";
             }
         }
+
+        function editarPrato(event) {
+            var pratoId = event.target.dataset.id;
+            var pratoRow = event.target.closest('tr');
+            var nome = pratoRow.querySelector('.nome').textContent;
+            var descricao = pratoRow.querySelector('.descricao').textContent;
+            var preco = pratoRow.querySelector('.preco').textContent;
+            var categoria = pratoRow.querySelector('.categoria').textContent;
+
+            document.getElementById('prato-form').setAttribute('data-id', pratoId);
+            document.getElementById('nome').value = nome;
+            document.getElementById('descricao').value = descricao;
+            document.getElementById('preco').value = preco;
+            document.getElementById('categoria').value = categoria;
+
+            // Atualizar o título do modal para "Editar Prato"
+            document.getElementById('modal-title').textContent = 'Editar Prato';
+
+            modal.style.display = "block";
+        }
+        function salvarPrato() {
+            var pratoId = document.getElementById('prato-form').getAttribute('data-id');
+            var nome = document.getElementById('nome').value;
+            var descricao = document.getElementById('descricao').value;
+            var preco = document.getElementById('preco').value;
+            var categoria = document.getElementById('categoria').value;
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    location.reload();
+                }
+            };
+
+            if (pratoId) { // Editar prato existente
+                xhttp.open("POST", "editar_prato_action.php", true);
+                xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhttp.send("id=" + pratoId + "&nome=" + nome + "&descricao=" + descricao + "&preco=" + preco + "&categoria=" + categoria);
+            } else { // Adicionar novo prato
+                xhttp.open("POST", "adicionar_prato_action.php", true);
+                xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhttp.send("nome=" + nome + "&descricao=" + descricao + "&preco=" + preco + "&categoria=" + categoria);
+            }
+        }
+
+
     </script>
 
 </body>
