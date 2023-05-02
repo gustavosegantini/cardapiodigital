5a1b24ea21152757fcfdb33e5a0d17cccb652ae0
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-05-02 12:25:35 -0300

    Update cardapio.php

--- cardapio.php
+++ cardapio.php
@@ -1,25 +1,19 @@
 <?php
+session_start();
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require '../conect.php';
 
-function obterPratos($restaurante_id)
-{
+function obterPratos($restaurante_id) {
     $conn = conectarBD();
     $sql = "SELECT * FROM pratos WHERE restaurante_id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $restaurante_id);
     $stmt->execute();
     $result = $stmt->get_result();
-
-    $pratos = [];
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $pratos[] = $row;
-        }
-    }
+    $pratos = $result->fetch_all(MYSQLI_ASSOC);
 
     $stmt->close();
     $conn->close();
@@ -27,44 +21,32 @@ function obterPratos($restaurante_id)
     return $pratos;
 }
 
+$restaurante_id = $_SESSION['restaurante_id'];
+$pratos = obterPratos($restaurante_id);
 
-$pratos = obterPratos();
 ?>
 
-
 <!DOCTYPE html>
-<html lang="pt-BR">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cardápio</title>
-    <!-- Inclua aqui o link para o CSS e qualquer outro recurso de estilo que você desejar -->
+    <!-- Adicione seus estilos e scripts aqui -->
 </head>
-
 <body>
     <h1>Cardápio</h1>
-    <div id="cardapio-container">
-        <?php foreach ($pratos as $prato): ?>
-            <div class="prato">
-                <h2>
-                    <?= $prato['nome'] ?>
-                </h2>
-                <p>
-                    <?= $prato['descricao'] ?>
-                </p>
-                <p>R$
-                    <?= number_format($prato['preco'], 2, ',', '.') ?>
-                </p>
-                <p>Categoria:
-                    <?= $prato['categoria'] ?>
-                </p>
-            </div>
-        <?php endforeach; ?>
 
-        <!-- Aqui vamos exibir os pratos do cardápio -->
-    </div>
-    <!-- Inclua aqui o link para o JavaScript e qualquer outro recurso de script que você desejar -->
+    <?php foreach ($pratos as $prato) { ?>
+        <div>
+            <h2><?php echo $prato["nome"]; ?></h2>
+            <p><?php echo $prato["descricao"]; ?></p>
+            <p>Preço: <?php echo $prato["preco"]; ?></p>
+            <p>Categoria: <?php echo $prato["categoria"]; ?></p>
+            <?php if ($prato["imagem"] !== null) { ?>
+                <img src="<?php echo $prato["imagem"]; ?>" alt="<?php echo $prato["nome"]; ?>">
+            <?php } ?>
+        </div>
+    <?php } ?>
 </body>
-
 </html>
